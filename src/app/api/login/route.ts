da697a000331c5e9613f41e56ed9e424d61afa8a
Asaad Hussain <asaadhussain2014@gmail.com>
2025-01-31 14:23:00 +0500

    Admin Panel

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -20,6 +20,6 @@ export async function POST(req: Request) {
 
     return NextResponse.json({ message: "Invalid credentials" }, { status: 401 });
   } catch (error) {
-    return NextResponse.json({ message: "Server error" }, { status: 500 });
+    return NextResponse.json({ message: "Server error", error }, { status: 500 });
   }
 }
