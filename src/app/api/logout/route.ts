5fdbee833681f90c57eccb37fc8068963ec2b37d
Asaad Hussain <asaadhussain2014@gmail.com>
2025-01-31 14:25:28 +0500

    Admin Panel

--- src/app/api/logout/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from "next/server";
-
-export async function GET() {
-  const response = NextResponse.redirect("/login");
-  response.headers.append("Set-Cookie", "auth_session=; Path=/; Max-Age=0");
-
-  return response;
-}
